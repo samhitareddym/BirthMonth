28e566f18d4b58d2acb76343addfae2f538e0977
samhitareddy <samhitareddy0402@gmail.com>
2024-09-30 12:11:02 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Create the Scanner object for input
+        Scanner in = new Scanner(System.in);
+        // Ask the user to enter their birth month
+        System.out.print("Enter your birth month (1-12): ");
+        int birthMonth = scanner.nextInt();
+        
+
+    }
+}
