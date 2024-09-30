20c5f79d3f5b2c91ba145ef02df72b6731af308d
samhitareddy <samhitareddy0402@gmail.com>
2024-09-30 15:57:30 -0400

    task 2

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Create the Scanner object for input
+        //Create a Scanner object to read input
         Scanner in = new Scanner(System.in);
         // Ask the user to enter their birth month
         System.out.print("Enter your birth month (1-12): ");
-        int birthMonth = scanner.nextInt();
-        
+        int birthMonth = in.nextInt();
+        // Check if the input is in the range 1 to 12
+        if (birthMonth >= 1 && birthMonth <= 12) {
+            //Print user's input if valid
+            System.out.println("Your birth month is: " + birthMonth);
+        } else {
+            // If not valid, output an error message
+            System.out.println("You entered an incorrect month value: " + birthMonth);
+        }
 
     }
 }
